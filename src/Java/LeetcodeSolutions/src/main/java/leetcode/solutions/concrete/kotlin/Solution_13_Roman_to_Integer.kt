8c4beaa009b5ae853ebcc6ca2931a74ba059c755
DanKPR <vaedermakar@mail.ru>
2022-08-07 16:12:23 +0300

    Rewriting solutions to Kotlin

---
+++ src/Java/LeetcodeSolutions/src/main/java/leetcode/solutions/concrete/kotlin/Solution_13_Roman_to_Integer.kt
@@ -0,0 +1,69 @@
+package leetcode.solutions.concrete.kotlin;
+
+import leetcode.solutions.*
+import leetcode.solutions.ProblemDifficulty.*
+import leetcode.solutions.SolutionValidator.*
+import leetcode.solutions.Complexity.*
+
+/**
+ * __Problem:__ Given a roman numeral, convert it to an integer.
+ *
+ * __Constraints:__
+ * - 1 <= num <= 3999
+
+ * __Solution 1:__
+ * - Iterate over the provided string, checking each character.
+ * - Increment the result variable by the appropriate amount, but with a check for the following situations:
+ *
+ * 1) I can be placed before V (5) and X (10) to make 4 and 9.
+ * 2) X can be placed before L (50) and C (100) to make 40 and 90.
+ * 3) C can be placed before D (500) and M (1000) to make 400 and 900.
+ *
+ * __Complexity:__ O(N)
+ * @author Daniil Kuprianov
+ */
+
+class Solution_13_Roman_to_Integer : LeetcodeSolution(EASY) {
+    init { resolveConcreteSolutionInfo(this) }
+
+    @ProblemSolution(complexity = O_N)
+    private fun romanToInt(s: String): Int {
+        var result = 0
+        var charBefore = '0'
+        for (i in s.indices) {
+            if (i != 0) charBefore = s[i - 1]
+            when (s[i]) {
+                'I' -> {
+                    ++result
+                }
+                'V' -> {
+                    result += if (charBefore == 'I') 3 else 5
+                }
+                'X' -> {
+                    result += if (charBefore == 'I') 8 else 10
+                }
+                'L' -> {
+                    result += if (charBefore == 'X') 30 else 50
+                }
+                'C' -> {
+                    result += if (charBefore == 'X') 80 else 100
+                }
+                'D' -> {
+                    result += if (charBefore == 'C') 300 else 500
+                }
+                'M' -> {
+                    result += if (charBefore == 'C') 800 else 1000
+                }
+            }
+        }
+        return result
+    }
+
+    @Override
+    @ProblemInputData
+    override fun run() {
+        ASSERT_EQ(3,    romanToInt("III"));
+        ASSERT_EQ(58,   romanToInt("LVIII"));
+        ASSERT_EQ(1994, romanToInt("MCMXCIV"));
+    }
+}
---
+++ src/Java/LeetcodeSolutions/src/main/java/leetcode/solutions/concrete/kotlin/Solution_9_Palindrome_Number.kt
@@ -0,0 +1,71 @@
+package leetcode.solutions.concrete.kotlin;
+
+import leetcode.solutions.*
+import leetcode.solutions.ProblemDifficulty.*
+import leetcode.solutions.SolutionValidator.*
+import leetcode.solutions.Complexity.*
+
+/**
+ * __Problem:__ Given an integer x, return true if x is palindrome integer.
+ * An integer is a palindrome when it reads the same backward as forward.
+ *
+ * __Constraints:__
+ * - -2^31 <= x <= 2^31 - 1
+
+ * __Solution 1:__
+ * - Convert a number to a string for iteration.
+ * - Iterate through the array to the middle using counters for the beginning and end, comparing the values.
+ *
+ * __Complexity:__ O(N)
+ *
+ * __Solution 2:__
+ * - Reverse half of x while dividing it to become the upper half.
+ * - In the end check if reversed lower half is equal to the above half, or (in case if it's an odd length palindrome)
+ * we check that equality with reversed rhs divided by 10 to skip that middle part.
+ *
+ * __Complexity:__ O(N)
+ * @author Daniil Kuprianov
+ */
+
+class Solution_9_Palindrome_Number : LeetcodeSolution(EASY) {
+    init { resolveConcreteSolutionInfo(this) }
+
+    @ProblemSolution(complexity = O_N)
+    private fun isPalindrome1(x: Int): Boolean {
+        if (x < 0 || (x > 0 && (x % 10 == 0))) return false
+        val asString = x.toString()
+        val digitsCount = asString.length
+        var i = 0; var j = asString.length - 1
+        while (i < digitsCount / 2) {
+            if (asString[i] != asString[j]) {
+                return false
+            }
+            ++i; --j
+        }
+        return true
+    }
+
+    @ProblemSolution(complexity = O_logN)
+    private fun isPalindrome2(x: Int): Boolean {
+        if (x < 0 || (x > 0 && (x % 10 == 0))) return false
+        var xCpy = x; var rhs = 0
+        while (xCpy > rhs) {
+            rhs *= 10
+            rhs += xCpy % 10
+            xCpy /= 10 // mutating x itself is not allowed in this magnificent language
+        }
+        return rhs == xCpy || rhs / 10 == xCpy
+    }
+
+    @Override
+    @ProblemInputData
+    override fun run() {
+        ASSERT_TRUE(isPalindrome1(321123));
+        ASSERT_TRUE(isPalindrome1(543212345));
+        ASSERT_FALSE(isPalindrome1(12345));
+
+        ASSERT_TRUE(isPalindrome2(12533521));
+        ASSERT_FALSE(isPalindrome2(543211234));
+        ASSERT_FALSE(isPalindrome2(12345));
+    }
+}
